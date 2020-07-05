4a4c353cb0859e4966687a356c94c39229a6a4d7
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-07-05 20:37:09 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name="Luis")
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name="Luis",  language="Pascal")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
